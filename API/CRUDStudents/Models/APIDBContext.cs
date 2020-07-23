c20eb452f056b0a863a6b52dd5be06ab69b1716f
Hager Hamdy <50157233+Hagerhamdy@users.noreply.github.com>
2020-07-23 06:02:06 +0200

    Add files via upload

---
+++ API/CRUDStudents/Models/APIDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDStudents.Models
+{
+    public class APIDBContext : DbContext
+    {
+        public APIDBContext(DbContextOptions<APIDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<StudentAccount> studentAccounts { get; set; }
+    
+    
+    }
+}
---
+++ API/CRUDStudents/Models/StudentAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDStudents.Models
+{
+    public class StudentAccount
+    {
+        [Key]
+        public int StudentAccountID { get; set; }
+        [Column(TypeName = "nvarchar(20)")]
+        [Required]
+        public string StudentNo { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string StudentName { get; set; }
+        [Required]
+        public int TeacherID { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
