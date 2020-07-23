3a0bda4911d674d570fb0b039d2a84dde770858e
Hager Hamdy <50157233+Hagerhamdy@users.noreply.github.com>
2020-07-23 06:00:21 +0200

    Add files via upload

---
+++ API/CRUDStudents/Migrations/20200722152706_app.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDStudents.Migrations
+{
+    public partial class app : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "studentAccounts",
+                columns: table => new
+                {
+                    StudentAccountID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentNo = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    StudentName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    TeacherID = table.Column<int>(nullable: false),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_studentAccounts", x => x.StudentAccountID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teachers",
+                columns: table => new
+                {
+                    TeacherID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherName = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teachers", x => x.TeacherID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "studentAccounts");
+
+            migrationBuilder.DropTable(
+                name: "teachers");
+        }
+    }
+}
